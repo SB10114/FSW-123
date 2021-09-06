d6c1ccef4cace40752af911573c3bede84691709
Sierra <sierra.butler@bryanuniversity.edu>
2021-09-05 22:03:08 -0500

    week 1 push

---
+++ my-todo/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+    return (
+      <form>
+        <h1>List of Todos</h1>
+        <ul>
+          <label>
+            <input type= "checkbox"></input>
+              FSW-123 homework
+          </label>
+          <br></br>
+          <label>
+            <input type= "checkbox"></input>
+              FSW-125 homework
+          </label>
+          <br></br>
+          <label>
+            <input type= "checkbox"></input>
+              Give the dog a bath
+          </label>
+          <br></br>
+          <label>
+            <input type= "checkbox"></input>
+              Do the laundry
+          </label>
+        </ul>
+      </form>
+    );
+  };
+
+export default App;
